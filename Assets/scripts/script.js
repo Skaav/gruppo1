e6f3a73cb1e53216693e1960afd0c5ab4f4ac770
andbardii <andbardii@icloud.com>
2023-04-20 18:54:35 +0200

    basics js

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,90 +1,109 @@
 //! SWITCH PAGES
-let start = document.querySelector('.nextPoint');
-    start.addEventListener('click',function(){
-        createButtons();
-        document.querySelector('.stage1').remove();
+// DIV TARGET
+let target = document.querySelector('.target');
+// console.log(target);
+
+// TEMPLATES
+
+let templates = document.querySelectorAll('template');
+console.log(templates);
+
+let firstTemplate = templates[0].content.cloneNode(true);
+let secondTemplate = templates[1].content.cloneNode(true);
+
+
+
+//! START PAGE
+//BUTTONS
+let startButton = document.querySelector('.startButton');
+// console.log(startButton);
+
+startButton.addEventListener('click',function(){
+    target.innerHTML = '';
+    target.append(firstTemplate)
+
     })
-//! TIMER (PAGINA 2)
-// {
-//     let semicirles = document.querySelectorAll(".semicircle");
-//     let timer = document.querySelector(".timer");
+
+//! FIRST AND SECOND PAGE
+let contatore = 0;
+let domande;
+let domandaCorrente;
+let risposteSbagliate = [];
+
+async function init(){
     
-//     //input
-//     let hr = 0;
-//     let min = 0;
-//     let sec = 60;
+    let apiUrl = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy';
+    domande = await fetch(apiUrl).then(res => res.json());//ottengo le domande
+    // domande = shuffle(domande.results);//mescolo le domande
     
-//     let hours = hr * 3600000;
-//     let minutes = min * 60000;
-//     let seconds = sec * 1000;
-//     let setTime = hours + minutes + seconds;
-//     let starTime = Date.now();
-//     let futureTime = starTime + setTime;
+    // console.log(domande);
     
+    let firstButton = firstTemplate.querySelector('.firstButton');
+    console.log(firstButton);
+    firstButton.addEventListener('click',function(){
+        // createButtons();
+        
+        target.innerHTML = '';
+        target.append(secondTemplate)
+    })
     
-//     let = timerLoop = setInterval(countDownTimer);
-//     countDownTimer();
+}
+
+// function createButtons(){
+
+
+//     domandaCorrente = domande[contatore];
+//     let {type,difficulty,question,correct_answer,incorrect_answers} = domandaCorrente;
+
     
-//     function countDownTimer() {
-//         let currentTime = Date.now();
-//         let remainingTime = futureTime - currentTime;
-//         let angle = (remainingTime / setTime) * 360;
-    
-//         //progress indicator
-//         if (angle > 180) {
-//             semicirles[2].style.display = 'none';
-//             semicirles[0].style.transform = 'rotate(180deg)';
-//             semicirles[1].style.transform = `rotate(${angle}deg)`;
-//         } else {
-//             semicirles[2].style.display = 'block';
-//             semicirles[0].style.transform = `rotate(${angle}deg)`;
-//             semicirles[1].style.transform = `rotate(${angle}deg)`;
-//         }
-    
-//         //timer
-//         let secs = Math.floor((remainingTime / 1000) % 120)
-    
-//         timer.innerHTML = `
-//         <div>${secs}</div>
-//         `;
-    
-//         //5 sec condition
-    
-//         //end
-//         if(remainingTime < 0) {
-//             clearInterval(timerLoop);
-//             semicirles[0].style.display = 'none';
-//             semicirles[1].style.display = 'none';
-//             semicirles[2].style.display = 'none';
-    
-//             timer.innerHTML = `
-//             <div>00</div>
-//             `;
-//         }
-//     }
+//     //inizio a selezionare gli elementi con cui interagire
+//     let titolo = document.querySelector('.stage2 .stage-title');
+//     let areaBottoni = document.querySelector('.stage2 .button-area');
+//     areaBottoni.innerHTML = '';
+
+//     //inizio a compilare
+//     titolo.textContent = question;
+
+//     let risposteCompleto = incorrect_answers;//assegno il valore ad un nuovo array che modificherò, lasciando intatto incorrect_answers
+//     risposteCompleto.push(correct_answer);
+
+//     if(type != 'boolean'){
+//         risposteCompleto = shuffle(risposteCompleto);//mescolo le risposte
 //     }
 
-//! DOUGHNUT (PAGINA 3)
-// {
-//     let canvas = document.getElementById('myCanvas').getContext('2d');
-    
-//     let lable = ['Correct', 'Wrong'];
-//     let data = [5, 7];
-//     let chart = new Chart(canvas, {
-//         type: 'doughnut',
-//         data: {
-//             lables: lable,
-//             datasets: [{
-//                 backgroundColor:[
-//                     "#D20094",
-//                     "#00ffff"
-//                 ],
-//                 borderWidth: 0,
-//                 cutout: 95, 
-//                 lable: 'Risposte',
-//                 data: data
-//             }],
-//         }
-    
-//     })
+//     for(let rispostaSingola of risposteCompleto){
+//         let button = document.createElement('button');
+//         button.textContent = rispostaSingola;
+//         button.classList.add('bottone-risposta');
+//         button.addEventListener('click',function(){
+//             if(domande.length < contatore){
+
+//                 contatore++;
+//                 if(incorrect_answers.includes(rispostaSingola)){
+//                     risposteSbagliate.push(domandaCorrente)
+//                 }
+//                 createButtons();
+//             }else{
+//                 //funzione che distrugge lo stage 2 e attiva lo stage 3
+//             }
+//         })
+//         areaBottoni.append(button)
 //     }
+
+//     console.log(risposteSbagliate);
+    
+
+// }
+
+// function shuffle(array) {
+//     let newArr = [];
+//     let length = array.length;
+//     for (let i = 0; i < length; i++) {
+//       let rand = Math.floor(Math.random() * array.length);
+//       newArr.push(array[rand]);
+//       array.splice(rand, 1);
+//     }
+//     return newArr;
+//   }
+
+init();
