d0407954790a922d817703fe4024d939452d3517
andbardii <andbardii@icloud.com>
2023-04-20 11:02:56 +0200

    id butto and js

--- Assets/scripts/javas.js
+++ Assets/scripts/javas.js
@@ -1,14 +1,15 @@
 let currentTemplate = 1;
-let carica = document.getElementsByClassName("nextPoint");
+let currentButton = 0;
+let button = document.getElementById("button" + currentButton);
 let targetDiv = document.getElementById("targetDiv");
-let currentButton = 1;
 
 
-carica.addEventListener("click", caricaTemplate);
+button.addEventListener("click", caricaTemplate);
 function caricaTemplate() {
     targetDiv.innerHTML = "";
     let template = document.getElementById("template" + currentTemplate);
     let clone = document.importNode(template.contentEditable, true);
     targetDiv.appendChild(clone);
     currentTemplate++;
+    currentButton++;
 }
